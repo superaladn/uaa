96c62fc9bd9798dbe523e99e576286438434e7a5
Brittany <brittany.johnson@ge.com>
2016-03-07 09:57:16 -0800

    [Sanjeev,Roop,Brittany,Vineet] Added Jwt bearer grant type.

--- model/src/main/java/org/cloudfoundry/identity/uaa/oauth/token/ClaimConstants.java
+++ model/src/main/java/org/cloudfoundry/identity/uaa/oauth/token/ClaimConstants.java
@@ -52,5 +52,10 @@ public class ClaimConstants {
     public static final String EXTERNAL_ATTR = "ext_attr";
     public static final String AMR = "amr";
     public static final String ACR = "acr";
+<<<<<<< 011e1b86340db0b9e098e7d1eeaf4b0af8797360
     public static final String PREVIOUS_LOGON_TIME = "previous_logon_time";
+||||||| merged common ancestors
+=======
+    public static final String TENANT_ID = "tenant_id";
+>>>>>>> [Sanjeev,Roop,Brittany,Vineet] Added Jwt bearer grant type.
 }
